921c4dfed59b75e2a0d9cde7688ec1b2a544345f
Yaugan <yaugansd45@gmail.com>
2020-05-23 09:03:12 +0530

    Sheep It Up

---
+++ GameProject 5- Sheep It Up/Assets/Scripts/Player Scripts/PlayerInteraction.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool playerDied;
+    private CameraFollow cameraFollow;
+
+    
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        cameraFollow = Camera.main.GetComponent<CameraFollow>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!playerDied)
+        {
+            if(rb.velocity.sqrMagnitude > 60)
+            {
+                playerDied = true;
+                cameraFollow.CanFollow = false;
+
+                SoundManager.instance.GameEndSound();
+                GameplayController.instance.RestartGame();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider target)
+    {
+        if(target.tag == "Coin")
+        {
+            target.gameObject.SetActive(false);
+
+            SoundManager.instance.PickedUpCoin();
+            GameplayController.instance.IncrementScore();
+        }
+
+        if(target.tag == "Spike")
+        {
+            cameraFollow.CanFollow = false;
+            gameObject.SetActive(false);
+
+            SoundManager.instance.GameEndSound();
+            GameplayController.instance.RestartGame();
+        }
+    }
+
+    private void OnCollisionEnter(Collision target)
+    {
+        if(target.gameObject.tag == "EndPlatform")
+        {
+            SoundManager.instance.GameStartSound();
+            GameplayController.instance.RestartGame();
+        }
+    }
+}
